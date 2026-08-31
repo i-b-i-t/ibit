847131a2f0e737c6dd8948ffde2e69617a66df12
LJM12914 <LJM12914@outlook.com>
2026-08-31 22:47:50 +0800

    Update revert.js

--- scripts/revert.js
+++ scripts/revert.js
@@ -4,7 +4,7 @@ export default function init() {}
 
 world.afterEvents.itemCompleteUse.subscribe(data=>{
     const { source, itemStack } = data;
-    if(source.typeId === "minecraft:player" && itemStack.typeId === "minecraft:enchanted_golden_apple") source.addEffect("minecraft:regeneration", 600, {
+    if(source.typeId === "minecraft:player" && itemStack.typeId === "minecraft:enchanted_golden_apple") source.addEffect("minecraft:regeneration", 1200, {
         amplifier: 4,
         showParticles: true
     });
